aaa6165cc369f3ca141d0ea9f0483a6ef11b851f
Jordan Amdahl <jrdnmdhl@gmail.com>
2018-05-27 10:28:47 -0400

    Removed check file, commented parameter sorting

--- R/misc.R
+++ R/misc.R
@@ -10,29 +10,58 @@
 #'
 #' @keyword internal
 sort.parameters <- function(x, ...) {
+
+  # Extract parameter names
   par_names <- names(x)
+
+  # Extract the names referenced in each parameter's
+  # formula
   var_list <- purrr::map(x, function(y) {
     vars <- all.vars(y$expr)
     vars[vars %in% par_names]
   })
+
+  # Define the lists of ordered and unordered parameters
   ordered <- c()
   unordered <- var_list
+
+  # While we still have parameters in the unordered list...
   while(length(unordered) > 0) {
+
+    # Define a vector which will hold the indices of each
+    # parameter to be moved to the ordered list
     to_remove <- c()
+
+    # Loop through each unordered parameter
     for(i in seq_len(length(unordered))) {
+
+      # If all the parameters its formula references
       if(all(unordered[[i]] %in% ordered)) {
+
+        # Append it to the list of ordered parameters
         ordered <- c(ordered, names(unordered)[i])
+
+        # and mark it  for remval
         to_remove <- c(to_remove, i)
       }
     }
+
     if(length(to_remove) == 0) {
+      # If we didn't find anything to move to the ordered list,
+      # throw a circular reference error
       stop('Circular reference in parameters', call. = F)
     } else {
+      # Otherwise, remove from the unordered list the parameters
+      # that were appended to the ordered list
       unordered <- unordered[-to_remove]
     }
   }
+
+  # Sort the parameters list according to the new order and
+  # apply its original class
   res <- x[ordered]
   class(res) <- class(x)
+
   res
 
 }
