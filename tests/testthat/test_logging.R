32caafe8b52bda0c5fb29a92f538b5b65513ec8c
Jordan Amdahl <jrdnmdhl@gmail.com>
2019-02-27 23:32:52 -0500

    More coverage

---
+++ tests/testthat/test_logging.R
@@ -0,0 +1,40 @@
+context("Logging")
+library(tibble)
+
+test_that("Logging a Simple Model", {
+  model <- list(
+    settings = tribble(
+      ~setting,       ~value,
+      "timeframe",         "72",
+      "timeframe_unit",      "Years",
+      "cycle_length",          "1",
+      "cycle_length_unit",      "Years",
+      "discount_cost",          "3",
+      "discount_outcomes",          "3",
+      "half_cycle_method", "life-table"
+    ),
+    variables = tribble(
+      ~name, ~strategy, ~group, ~formula,
+      'x'  , NA,        NA,     'y + 1',
+      'y'  , NA,        NA,     'z + 2',
+      'z'  , NA,        NA,     '30',
+      'a'  , NA,        NA,     'mtcars',
+      'b'  , NA,        NA,     'lm(mpg~disp, data = a)',
+      'c'  , NA,        NA,     'predict(b, newdata = data.frame(disp = x))',
+      'i'  , NA,        NA,     'model_time + c'
+    ),
+    strategies = tribble(~name, 'strat1', 'strat2', 'strat3'),
+    groups = tribble(
+      ~name,     ~weight,
+      'group_a', '0.5',
+      'group_b', '0.3',
+      'group_c', '0.2'
+    )
+  ) 
+  testthat::expect_output(
+    evaluate_model(model, log = list(level = 1, type = 'console')),
+    'Evaluating strategy'
+  )
+  
+  
+})
--- tests/testthat/test_look_up.R
+++ tests/testthat/test_look_up.R
@@ -1,6 +1,6 @@
 context("Lookup")
 
-test_that("Lookups", {
+test_that("Basic Lookups", {
   
   expect_equal(
     look_up(airquality, Month = 8, Day = 1, value = 'Wind'),
@@ -18,8 +18,52 @@ test_that("Lookups", {
   )
   
   expect_equal(
+    look_up(airquality, Month = 6, Day = 1.2, value = 'Temp', bin = 'Day'),
+    78
+  )
+  
+  expect_equal(
     look_up(CO2, Plant = "Qn1", conc = 95, value = 'uptake'),
     16
+  )
+  
+})
+
+test_that("Lookup Erros", {
+  
+  expect_error(
+    look_up(as.list(CO2), Plant = "Qn1", conc = 95, value = 'uptake'),
+    'must be a data.frame'
+  )
+  
+  expect_error(
+    look_up(CO2, Plant = "Qn1", conc = 95, 40, value = 'uptake'),
+    'must be named'
+  )
+  
+  expect_error(
+    look_up(CO2, Plant = "Qn1", conc = 95, blah = 40, value = 'uptake'),
+    "not found in 'data'"
+  )
+  
+  expect_error(
+    look_up(airquality, Month = 6, Day = 1.2, value = 'Temp', bin = 'blah'),
+    "Names in 'bin' not found in source data"
+  )
+  
+  expect_error(
+    look_up(CO2, Plant = "Qn1", conc = 95, value = 'uptake', bin = "Plant"),
+    "Some variables in 'bin' are not numeric"
+  )
+  
+  expect_error(
+    look_up(mtcars, cyl = 6, hp = 110, value = 'qsec'),
+    "Some rows in 'data' are duplicates"
+  )
+  
+  expect_warning(
+    look_up(CO2, Plant = "blah", conc = 95, value = 'uptake'),
+    "Some values were not found"
   )
   
 })
