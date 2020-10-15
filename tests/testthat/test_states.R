0ba7c4f71379ea1305b6258e6fb730044ebe19a7
Alex Lonshteyn <alonshteyn@gmail.com>
2020-10-15 16:00:55 -0400

    Updated test_states.xlsx and tes_states.r

--- tests/testthat/test_states.R
+++ tests/testthat/test_states.R
@@ -5,16 +5,18 @@ library(heRomod2)
 
 # Prepare data for test cases
 state_tests <- system.file("test_cases", "test_states.xlsx", package = "heRomod2") %>%
-  read_workbook()
+  read_workbook() 
 
 segment <- tibble::tibble(strategy = "S1", group = "G1")
 test_ns <- heRomod2:::create_test_ns(segment)
 cl_unit_code <- 'cycle_length_unit'
-settings <- parse_settings(state_tests$setting)
+settings <- heRomod2:::parse_settings(state_tests$setting)
 
 
 test_that('states are  properly load', {
-  expect_success(res <- heRomod2:::parse_states(state_tests$states, settings))
+  expect_error(
+    res <- heRomod2:::parse_states(state_tests$states, settings), 
+    NA)
 })
 
 test_that('duplicate state names are detected', {
@@ -22,6 +24,27 @@ test_that('duplicate state names are detected', {
   # Parse the variables specification
   expect_error(
     heRomod2:::parse_states(state_tests$dup_names, settings),
-    'factor level [2] is duplicated'
+    'factor level [2] is duplicated', fixed=TRUE
+  )
+})
+
+
+
+test_that('missing state names are detected', {
+  
+  # Parse the variables specification
+  expect_error(
+    heRomod2:::parse_states(state_tests$miss_name, settings),
+    'factor level [2] is duplicated', fixed=TRUE
+  )
+})
+
+
+test_that('wrong state names are detected', {
+  
+  # Parse the variables specification
+  expect_error(
+    heRomod2:::parse_states(state_tests$wrong_name, settings),
+    'factor level [2] is duplicated', fixed=TRUE
   )
 })
